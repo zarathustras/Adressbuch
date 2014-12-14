ee95f0d5994156bba5fec632344adee4b777f7e6
Rudolf Nitsche <rn@MacBookAir.local>
2014-12-14 06:33:58 +0100

    Initial Commit

---
+++ Adressbuch/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Adressbuch
+//
+//  Created by Rudolf Nitsche on 14/12/14.
+//  Copyright (c) 2014 red. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
